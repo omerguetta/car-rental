a19ab7e6369739b6ff446602604e097f818abef7
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-02-03 11:56:11 +0200

    Create CarGrid and add functionality

---
+++ src/components/CarGrid/CarGrid.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CarCard from "../CarCard/CarCard";
+import "./CarGrid.css";
+
+const CarGrid = ({ cars, onToggleFavorite, favorites }) => {
+  return (
+    <div className="car-grid-container">
+      <div className="car-grid-header">
+        <h2 className="grid-title">Cars Catalogue</h2>
+        <span className="grid-count">{cars.length} Cars</span>
+      </div>
+
+      <div className="car-grid">
+        {cars.length > 0 ? (
+          cars.map((car) => (
+            <CarCard key={car.id} car={{ ...car, favorite: favorites[car.id] || false }} onToggleFavorite={onToggleFavorite} />
+          ))
+        ) : (
+          <p className="loading-text">No cars available</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CarGrid;
